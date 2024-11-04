9f1bec73532625de05811c948a2ce9e6326871a8
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-04 00:11:09 -0300

    create User class

---
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/User.java
@@ -0,0 +1,72 @@
+package com.gleidsonsilva.restapi_dio_desafio.domain.model;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String name;
+    private Account account;
+    private Card card;
+    private List<Feature> features;
+    private List<News> news;
+
+    public User() {
+    }
+
+    public User(Long id, String name, Account account, Card card, List<Feature> features, List<News> news) {
+        this.id = id;
+        this.name = name;
+        this.account = account;
+        this.card = card;
+        this.features = features;
+        this.news = news;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
