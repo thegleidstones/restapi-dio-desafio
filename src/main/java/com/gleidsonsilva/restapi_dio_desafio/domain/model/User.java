48a4a95d0c1b9c0c73f54229204bcc689fa69442
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-04 00:40:23 -0300

    add orm map with jpa to user class

--- src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/User.java
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/User.java
@@ -1,13 +1,28 @@
 package com.gleidsonsilva.restapi_dio_desafio.domain.model;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
+@Entity(name = "tb_users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<News> news;
 
     public User() {
