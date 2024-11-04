9c7e6f2eb7894097ad90c9009e7ab825a0862495
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-03 23:23:04 -0300

    Initial commit

---
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/RestapiDioDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.gleidsonsilva.restapi_dio_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiDioDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiDioDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gleidsonsilva/restapi_dio_desafio/RestapiDioDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleidsonsilva.restapi_dio_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiDioDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
