d074ba6547c5af00a471dd12fd0a0e2468bd937f
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-04 00:40:41 -0300

    add orm map with jpa to news class

--- src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/Feature.java
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/Feature.java
@@ -1,40 +1,8 @@
 package com.gleidsonsilva.restapi_dio_desafio.domain.model;
 
-public class Feature {
-    private Long id;
-    private String icon;
-    private String description;
+import jakarta.persistence.Entity;
 
-    public Feature() {
-    }
+@Entity(name = "tb_features")
+public class Feature extends  BaseItem{
 
-    public Feature(Long id, String icon, String description) {
-        this.id = id;
-        this.icon = icon;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/News.java
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/News.java
@@ -1,40 +1,8 @@
 package com.gleidsonsilva.restapi_dio_desafio.domain.model;
 
-public class News {
-    private Long id;
-    private String icon;
-    private String description;
+import jakarta.persistence.Entity;
 
-    public News() {
-    }
+@Entity(name = "tb_news")
+public class News extends BaseItem{
 
-    public News(Long id, String icon, String description) {
-        this.id = id;
-        this.icon = icon;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
