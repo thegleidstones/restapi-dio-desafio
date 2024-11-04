56aeaa8823ad9ce495297a6a835e6d176d8cabbe
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-04 00:43:24 -0300

    create base class to news and features

---
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/BaseItem.java
@@ -0,0 +1,40 @@
+package com.gleidsonsilva.restapi_dio_desafio.domain.model;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public class BaseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String icon;
+
+    @Column(nullable = false)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
