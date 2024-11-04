e2c99adf0682246be08d8f50647ef77bf9e93d68
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-04 00:11:49 -0300

    create Feature class

---
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/Feature.java
@@ -0,0 +1,40 @@
+package com.gleidsonsilva.restapi_dio_desafio.domain.model;
+
+public class Feature {
+    private Long id;
+    private String icon;
+    private String description;
+
+    public Feature() {
+    }
+
+    public Feature(Long id, String icon, String description) {
+        this.id = id;
+        this.icon = icon;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
