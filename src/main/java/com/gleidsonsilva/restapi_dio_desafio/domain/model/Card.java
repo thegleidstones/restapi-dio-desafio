de6447115061080c9ab2af3dcefbdeb495054339
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-04 00:11:36 -0300

    create card class

---
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/model/Card.java
@@ -0,0 +1,42 @@
+package com.gleidsonsilva.restapi_dio_desafio.domain.model;
+
+import java.math.BigDecimal;
+
+public class Card {
+    private Long id;
+    private String number;
+    private BigDecimal limit;
+
+    public Card() {
+    }
+
+    public Card(Long id, String number, BigDecimal limit) {
+        this.id = id;
+        this.number = number;
+        this.limit = limit;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
