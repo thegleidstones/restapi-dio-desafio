f0604da1437b4dfbd72df6bd1c144e1005508a09
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-04 00:43:07 -0300

    create user repository class

---
+++ src/main/java/com/gleidsonsilva/restapi_dio_desafio/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gleidsonsilva.restapi_dio_desafio.domain.repository;
+
+import com.gleidsonsilva.restapi_dio_desafio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
